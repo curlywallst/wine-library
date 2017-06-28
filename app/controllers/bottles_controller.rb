271d7cc6c3c664a0b38d767dd2a8b8ef8942cdce
curlywallst <nnoyes@comcast.net>
2017-06-28 15:49:10 -0400

    winery routes added

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,7 +2,6 @@ class BottlesController < ApplicationController
 
   get '/bottles' do
     @bottles = Bottle.all
-
     erb :'bottles/index'
   end
 
@@ -16,13 +15,35 @@ class BottlesController < ApplicationController
     @bottle.winery_id = winery.id
     @bottle.owner_id = current_user.id
     @bottle.save
+    redirect "/bottles/#{@bottle.id}"
+  end
 
+  get '/bottles/:id/edit' do
+    @bottle = Bottle.find(params[:id])
+    erb :'bottles/edit'
+  end
+
+  patch '/bottles/:id'do
+    @bottle = Bottle.find(params[:id])
+    @bottle.wine_type = params[:wine_type]
+    @bottle.year = params[:year]
+    @bottle.price = params[:price]
+    @bottle.winery_id = params[:winery_id]
+    @bottle.save
     redirect "/bottles/#{@bottle.id}"
   end
 
   get '/bottles/:id'do
     @bottle = Bottle.find(params[:id])
     erb :'bottles/show'
+  end
+
+  delete '/bottles/:id/delete' do
+    @bottle = Bottle.find(params[:id])
+    if @bottle.owner_id == current_user.id
+      @bottle.delete
+      redirect "/bottles"
+    end
   end
 
   helpers do
