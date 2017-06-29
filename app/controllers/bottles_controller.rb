1243602af9fb0bba6b9a89a446da02e8b4291fa9
curlywallst <nnoyes@comcast.net>
2017-06-29 15:08:10 -0400

    Added ability to make new bottle from existing wine type and or winery

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -6,12 +6,26 @@ class BottlesController < ApplicationController
   end
 
   get '/bottles/new' do
+    bottles = Bottle.all
+    @bottle_types = bottles.uniq{|x| x.wine_type}
+    @bottle_wineries = bottles.uniq{|x| x.winery_id}
     erb :'bottles/new'
   end
 
   post '/bottles' do
-    @bottle = Bottle.create(:wine_type => params[:wine_type], :price => params[:price], :year => params[:year])
-    winery = Winery.find_or_create_by(:name => params[:winery])
+    if !!params[:type]
+      wine_type = params[:type]
+    else
+      wine_type = params[:wine_type]
+    end
+
+    if !!params[:winery_name]
+      winery_name = params[:winery_name]
+    else
+      winery_name = params[:winery]
+    end
+    @bottle = Bottle.create(:wine_type => wine_type, :price => params[:price], :year => params[:year])
+    winery = Winery.find_or_create_by(:name => winery_name)
     @bottle.winery_id = winery.id
     @bottle.owner_id = current_user.id
     winery.bottles << @bottle
@@ -21,11 +35,16 @@ class BottlesController < ApplicationController
 
   get '/bottles/:id/edit' do
     @bottle = Bottle.find(params[:id])
-    erb :'bottles/edit'
+    if @bottle.owner_id == current_user.id
+      erb :'bottles/edit'
+    else
+      erb :index
+    end
   end
 
   patch '/bottles/:id'do
     @bottle = Bottle.find(params[:id])
+    binding.pry
     @bottle.wine_type = params[:wine_type]
     @bottle.year = params[:year]
     @bottle.price = params[:price]
@@ -44,6 +63,8 @@ class BottlesController < ApplicationController
     if @bottle.owner_id == current_user.id
       @bottle.delete
       redirect "/bottles"
+    else
+      erb :'index'
     end
   end
 
